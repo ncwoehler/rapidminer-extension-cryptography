f224551f7212fbffc7b914632476caab8b9c816b
Nils Woehler <ncwoehler@gmail.com>
2014-02-08 16:40:45 +0100

    Added licenses to java classes, include checkstyle with build.gradle

--- src/main/java/com/rapidminer/operator/PBEAlgorithmParameterHandler.java
+++ src/main/java/com/rapidminer/operator/PBEAlgorithmParameterHandler.java
@@ -1,3 +1,21 @@
+/*
+ *  RapidMiner Encryption Extension
+ *
+ *  Copyright (C) 2014 by Nils Wöhler
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
 package com.rapidminer.operator;
 
 import java.util.LinkedList;
@@ -14,6 +32,11 @@ import com.rapidminer.parameter.ParameterTypeSuggestion;
 import com.rapidminer.parameter.UndefinedParameterError;
 import com.rapidminer.parameter.conditions.EqualTypeCondition;
 
+/**
+ * 
+ * @author Nils Woehler
+ *
+ */
 public class PBEAlgorithmParameterHandler {
 
 	public static final String PARAMETER_PASSWORD = "password";
--- src/main/java/com/rapidminer/operator/text/AbstractPBDocumentEncryptionOperator.java
+++ src/main/java/com/rapidminer/operator/text/AbstractPBDocumentEncryptionOperator.java
@@ -1,3 +1,21 @@
+/*
+ *  RapidMiner Encryption Extension
+ *
+ *  Copyright (C) 2014 by Nils Wöhler
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
 package com.rapidminer.operator.text;
 
 import java.security.Security;
@@ -5,10 +23,10 @@ import java.util.List;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
-import com.rapidminer.operator.PBEAlgorithmParameterHandler;
 import com.rapidminer.operator.Operator;
 import com.rapidminer.operator.OperatorDescription;
 import com.rapidminer.operator.OperatorException;
+import com.rapidminer.operator.PBEAlgorithmParameterHandler;
 import com.rapidminer.operator.ProcessSetupError.Severity;
 import com.rapidminer.operator.SimpleProcessSetupError;
 import com.rapidminer.operator.UserError;
